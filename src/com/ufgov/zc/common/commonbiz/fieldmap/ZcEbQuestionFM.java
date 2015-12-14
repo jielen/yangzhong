77af358f230546c4fc663ec2cd48101f2173c4e8
jielen <jielenzghsy1@163.com>
2015-12-15 03:32:24 +0800

    dd

---
+++ src/com/ufgov/zc/common/commonbiz/fieldmap/ZcEbQuestionFM.java
@@ -0,0 +1,35 @@
+package com.ufgov.zc.common.commonbiz.fieldmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ZcEbQuestionFM {
+
+  public static Map fieldMap = new HashMap();
+
+  static {
+    fieldMap.put("HY_CONTENT", "hyContent");
+    fieldMap.put("HY_DATE", "hyDate");
+    fieldMap.put("HY_FILE", "hyFile");
+    fieldMap.put("HY_FILE_BLOBID", "hyFileBlobid");
+    fieldMap.put("INPUTOR", "inputor");
+    fieldMap.put("INPUT_DATE", "inputDate");
+    fieldMap.put("ND", "nd");
+    fieldMap.put("PROCESS_INST_ID", "processInstId");
+    fieldMap.put("PROJ_CODE", "projCode");
+    fieldMap.put("PROJ_JBR", "projJbr");
+    fieldMap.put("PROJ_NAME", "projName");
+    fieldMap.put("REMARK", "remark");
+    fieldMap.put("STATUS", "status");
+    fieldMap.put("ZY_CODE", "zyCode");
+    fieldMap.put("ZY_CONTENT", "zyContent");
+    fieldMap.put("ZY_DATE", "zyDate");
+    fieldMap.put("ZY_FILE", "zyFile");
+    fieldMap.put("ZY_FILE_BLOBID", "zyFileBlobid");
+    fieldMap.put("ZY_LINK_MAN", "zyLinkMan");
+    fieldMap.put("ZY_LINK_TEL", "zyLinkTel");
+    fieldMap.put("ZY_UNIT", "zyUnit");
+    fieldMap.put("ZY_TYPE", "zyType");
+
+  }
+}
---
+++ src/com/ufgov/zc/server/system/workflow/ZcEbQuestionHyWorkFlowLisenter.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.ufgov.zc.server.system.workflow;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.kingdrive.workflow.context.WorkflowContext;
+import com.kingdrive.workflow.exception.WorkflowException;
+import com.kingdrive.workflow.listener.TaskAdapter;
+import com.ufgov.zc.common.zc.model.ZcEbQuestionWithBLOBs;
+import com.ufgov.zc.server.system.SpringContext;
+import com.ufgov.zc.server.zc.dao.IZcEbBaseServiceDao;
+
+/**
+ * 回应质疑时，更新回应时间
+ * @author Administrator
+ */
+public class ZcEbQuestionHyWorkFlowLisenter extends TaskAdapter {
+
+  public void afterExecution(WorkflowContext context) throws WorkflowException {
+    super.afterExecution(context);
+    Long processId = context.getInstanceId();
+
+    IZcEbBaseServiceDao zcEbBaseServiceDao = (IZcEbBaseServiceDao) SpringContext.getBean("zcEbBaseServiceDao");
+
+    ZcEbQuestionWithBLOBs q = (ZcEbQuestionWithBLOBs) zcEbBaseServiceDao.queryObject("com.ufgov.zc.server.zc.dao.ZcEbQuestionMapper.selectByProcessInstId", "" + processId);
+
+    Date sysDate = (Date) zcEbBaseServiceDao.queryObject("ZcEbPlan.getSysdate", null);
+
+    q.setHyDate(sysDate);
+
+    List objLst = new ArrayList();
+    objLst.add(q);
+
+    zcEbBaseServiceDao.updateObjectList("com.ufgov.zc.server.zc.dao.ZcEbQuestionMapper.updateHyDate", objLst);
+
+  }
+
+}
